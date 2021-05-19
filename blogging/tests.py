669fa40e91240b8512de4db3f3b8f02e29b85e2f
Shadle Stewart <shadlestewart@gmail.com>
2021-05-18 20:28:47 -0700

    Added detail templates for blog posts, links to them, and related tests for the blogging app.

--- blogging/tests.py
+++ blogging/tests.py
@@ -55,3 +55,14 @@ class FrontEndTestCase(TestCase):
                 self.assertContains(resp, title, count=1)
             else:
                 self.assertNotContains(resp, title)
+
+    def test_details_only_published(self):
+        for count in range(1, 11):
+            title = f"Post {count} Title"
+            post = Post.objects.get(title=title)
+            resp = self.client.get(f'/posts/{post.pk}/')
+            if count < 6:
+                self.assertEqual(resp.status_code, 200)
+                self.assertContains(resp, title)
+            else:
+                self.assertEqual(resp.status_code, 404)
