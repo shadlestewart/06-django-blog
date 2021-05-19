90074d6f7e47d59b10a6ae013aa65febdceccc0a
Shadle Stewart <shadlestewart@gmail.com>
2021-05-18 19:52:40 -0700

    Added a list_view for the blogging app and a test to test showing the published posts.

--- blogging/tests.py
+++ blogging/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from blogging.models import Post, Category
+import datetime
+from django.utils.timezone import utc
 
 
 class PostTestCase(TestCase):
@@ -23,3 +25,33 @@ class CategoryTestCase(TestCase):
         c1 = Category(name=expected)
         actual = str(c1)
         self.assertEqual(expected, actual)
+
+
+class FrontEndTestCase(TestCase):
+    """test views provided in the front-end"""
+    fixtures = ['blogging_test_fixture.json', ]
+
+    def setUp(self):
+        self.now = datetime.datetime.utcnow().replace(tzinfo=utc)
+        self.timedelta = datetime.timedelta(15)
+        author = User.objects.get(pk=1)
+        for count in range(1, 11):
+            post = Post(title=f"Post {count} Title",
+                        text="foo",
+                        author=author)
+            if count < 6:
+                pubdate = self.now - self.timedelta * count
+                post.published_date = pubdate
+            post.save()
+
+    def test_list_only_published(self):
+        resp = self.client.get('/')
+        # content of rendered response is always a bytestring so need to decode
+        resp_text = resp.content.decode(resp.charset)
+        self.assertTrue("Recent Posts" in resp_text)
+        for count in range(1, 11):
+            title = f"Post {count} Title"
+            if count < 6:
+                self.assertContains(resp, title, count=1)
+            else:
+                self.assertNotContains(resp, title)
--- blogging/views.py
+++ blogging/views.py
@@ -1,4 +1,6 @@
 from django.http.response import HttpResponse, HttpResponseRedirect, Http404
+from django.template import loader
+from blogging.models import Post
 from django.shortcuts import render
 
 
@@ -11,3 +13,12 @@ def stub_view(request, *args, **kwargs):
         body += "Kwargs: \n"
         body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
     return HttpResponse(body, content_type="text/plain")
+
+
+def list_view(request):
+    published = Post.objects.exclude(published_date__exact=None)
+    posts = published.order_by('-published_date')
+    template = loader.get_template('blogging/list.html')
+    context = {'posts': posts}
+    body = template.render(context)
+    return HttpResponse(body, content_type="text/html")
