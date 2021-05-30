9fce6dd66263857c2a601df0cecb5077e684dcc5
Shadle Stewart <shadlestewart@gmail.com>
2021-05-30 12:29:02 -0700

    Changed blogging views to be class-based. The posts are displaying in reverse chronological order and are not showing up if they don't have a published date.

--- blogging/views.py
+++ blogging/views.py
@@ -2,6 +2,8 @@ from django.http.response import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
 from blogging.models import Post
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 
 def stub_view(request, *args, **kwargs):
@@ -15,21 +17,12 @@ def stub_view(request, *args, **kwargs):
     return HttpResponse(body, content_type="text/plain")
 
 
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    # template = loader.get_template('blogging/list.html')
-    context = {'posts': posts}
-    # body = template.render(context)
-    # return HttpResponse(body, content_type="text/html")
-    return render(request, 'blogging/list.html', context)
+class BlogListView(ListView):
+    queryset = Post.objects.exclude(published_date__exact=None)\
+        .order_by('-published_date')
+    template_name = 'blogging/list.html'
 
 
-def detail_view(request, post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    return render(request, 'blogging/detail.html', context)
+class BlogDetailView(DetailView):
+    queryset = Post.objects.exclude(published_date__exact=None)
+    template_name = 'blogging/detail.html'
